33f61ed93c76a57aa3b5c90a65c94b922db79c01
Dean Agan <agandfr@gmail.com>
2020-02-19 23:38:17 +1100

    Added main folder, and updaed BUILD files

---
+++ state_pattern/main/test.cpp
@@ -0,0 +1,14 @@
+
+
+#include "lib/EarningStatusContext.h"
+#include <iostream>
+#include <memory>
+
+int main()
+{
+   // auto esc = std::make_unique<FrequentFlyers::EarningStatusContext>();
+
+    //State::HelloClass hc;
+    std::cout << "Hello World!\n";
+    //std::cout << hc.Hello("Hose") << "\n";
+}
