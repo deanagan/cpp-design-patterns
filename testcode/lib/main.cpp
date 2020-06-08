6961ad8d3f6ab25837ddd85d3d869d5f090622ab
Dean Agan <agandfr@gmail.com>
2020-06-08 20:20:27 +1000

    Add solution

--- testcode/lib/main.cpp
+++ testcode/lib/main.cpp
@@ -1,5 +1,9 @@
 #include "main.h"
 #include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <iterator>
 
 namespace TestCode
 {
@@ -8,6 +12,38 @@ namespace TestCode
     }
 
     int MathOp::NearestValue(const std::vector<int>& numbers, int search) const {
-        return 0;
+        std::vector<int> sortedNumbers(numbers);
+        std::sort(begin(sortedNumbers), end(sortedNumbers), [search](int a, int b) -> bool {
+            const auto adiff = std::abs(a - search);
+            const auto bdiff = std::abs(b - search);
+
+            const auto res = (adiff - bdiff);
+            if (res == 0) {
+                return a < b;
+            }
+
+            return adiff < bdiff;
+        });
+        return sortedNumbers.front();
     }
+}
+
+int main()
+{
+    std::vector<int> sortedNumbers{4, 7, 10, 11, 12, 17};
+
+    int search = 9;
+
+    std::sort(begin(sortedNumbers), end(sortedNumbers), [search](int a, int b) -> bool {
+            const auto adiff = std::abs(a - search);
+            const auto bdiff = std::abs(b - search);
+
+            const auto res = (adiff - bdiff);
+            if (res == 0) {
+                return a < b;
+            }
+            std::cout << "{" << a << "," << b << "}\n";
+            return adiff < bdiff;
+        });
+        std::copy(begin(sortedNumbers), end(sortedNumbers), std::ostream_iterator<int>(std::cout, ", ") );
 }
